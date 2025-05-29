c63a28d0a0e066780e6f8ec9ade50c9d5a79d08b
EVANDRO <evandrojuniorklein@gmail.com>
2025-05-29 11:26:50 -0300

    Adicionando arquivos HTML,CSS,JAVASCRIPT e uma imagem.gif

---
+++ Bla.js
@@ -0,0 +1,11 @@
+let listaGenérica = [];
+let linguagensDeProgramaçao = ['JavaScript','C','C++', 'Kotlin' , 'Python'];
+
+linguagensDeProgramaçao.push('Java');
+linguagensDeProgramaçao.push('Ruby');
+linguagensDeProgramaçao.push('GoLang');
+
+let listaNome= ['Cleide', 'Carlos', 'Dalva'];
+console.log(listaNome[0]);
+console.log(listaNome[1]);
+console.log(listaNome[listaNome.length - 1]);
