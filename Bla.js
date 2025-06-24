0eabdd30bf7378420ea04c8417a6d05efb559328
EVANDRO <evandrojuniorklein@gmail.com>
2025-06-24 11:06:05 -0300

    alterações de volume e visuais

--- Bla.js
+++ Bla.js
@@ -1,3 +1,7 @@
+let tocouDoom = false;
+let tocouDirgeDisunion = false;
+let dirgeDisunion;
+let theOnlyThingTheyFearIsYou;
 
 function mostrarLista(){
     let listaAmmo= ['Pistol Ammo', 'Chaingun Ammo', 'Shells', 'Box of Shells'];
@@ -13,22 +17,31 @@ function mostrarLista(){
 
 function doom(n){
     let doomp = document.querySelector('.doomsection');
-    let theOnlyThingTheyFearIsYou = new Audio('Music/The-Only-Thing-They-Fear-is-You-Mick-Gordon.mp3');
-   
-    switch(n){
-        case 1:
-            theOnlyThingTheyFearIsYou.src = 'Music/The-Only-Thing-They-Fear-is-You-Mick-Gordon.mp3';
-        break;
+
+    if (!tocouDoom) {
+        tocouDoom = true;
+        theOnlyThingTheyFearIsYou = new Audio('Music/The-Only-Thing-They-Fear-is-You-Mick-Gordon.mp3');
+        theOnlyThingTheyFearIsYou.loop = true;
+        theOnlyThingTheyFearIsYou.volume = parseFloat(document.getElementById('volumeRange').value);
+
+        theOnlyThingTheyFearIsYou.play().catch(err => {
+            console.error('Erro ao tocar o áudio:', err);
+        });
+
+        // Atualiza volume em tempo real com o slider
+        document.getElementById('volumeRange').addEventListener('input', function () {
+            if (theOnlyThingTheyFearIsYou) {
+                theOnlyThingTheyFearIsYou.volume = parseFloat(this.value);
+            }
+        });
     }
 
-    theOnlyThingTheyFearIsYou.play();
     if (doomp) {
         let doomText = document.createElement('p');
         doomText.textContent = 'DOOM';
         doomText.classList.add('doomp');
         doomp.appendChild(doomText);        
     }
-
 }
 
 function adicionar(n){
@@ -65,13 +78,33 @@ function adicionar(n){
     }
 }
 
-function descarregar(){
+function descarregar(n){
+    if(!tocouDirgeDisunion){
+        tocouDirgeDisunion = true;
+        dirgeDisunion = new Audio('Music/Disunion-Dirge.mp3');
+        dirgeDisunion.loop = true;
+        dirgeDisunion.volume = parseFloat(document.querySelector('#volumeRange').value);
 
+        switch(n){
+            case 1:
+                dirgeDisunion.src = 'Music/Disunion-Dirge.mp3';
+                break;
+        }
+
+        dirgeDisunion.play().catch(err => {
+            console.error('Erro ao tocar a música:', err);
+        });
+    }
     let descarregarArmas = document.querySelector('.doomsection');
 
     if(descarregarArmas){
         descarregarArmas.innerHTML = '';
         alert('Munições Descarregadas')
     }
+    document.getElementById('volumeRange').addEventListener('input', function () {
+    if (dirgeDisunion) {
+        dirgeDisunion.volume = parseFloat(this.value);
+    }
+});
 
 }
